fc475c2aa4810c8ec7fbda4abd79dc630f81fefd
Snehauser <103923649+Snehauser@users.noreply.github.com>
2024-02-20 21:10:56 +0530

    Final streamlit_app.py
    Updated UI

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,6 @@ Created on Wed Feb 14 21:26:43 2024
 """
 import numpy as np
 import streamlit as st
-import pandas as pd
 import pickle
 
 pickle_in = open('parameters.pkl', 'rb')
@@ -20,6 +19,8 @@ acc = pickle.load(pickle_in2)
 
 
 #from PIL import image
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
 def predict(W1, W2, b1, b2, input):
   neth1 = np.dot(input, W1) + b1
@@ -31,7 +32,7 @@ def predict(W1, W2, b1, b2, input):
 
 
 def main():
-    st.title('Assignment_1')
+    st.title('CS772 Assignment 1')
     #st.image('Screenshot_2024-02-13_232956.png', caption='Neural Network Architecture')
     html_temp = """
     <div style="background-color:tomato;padding:10px">
@@ -39,25 +40,44 @@ def main():
     </div>
     """
     st.markdown(html_temp, unsafe_allow_html=True)
-    for i in range(4):
-      st.write(i+1, " fold accuracy (#inputs = 256): ", acc1[i])
+    #for i in range(4):
+      #st.write(i+1, " fold accuracy (#inputs = 256): ", acc1[i])
 
-    st.write("Whole dataset accuracy (#inputs = 1024): ", acc)
+    #st.write("Whole dataset accuracy (#inputs = 1024): ", acc)
 
-    st.write("Following results are final weights and biases")
-    st.write(param)
+    #st.write("Following results are final weights and biases")
+    #st.write(param)
     
-    num = st.text_input("Enter number: ", "Type here")
-    inp = np.array([int(digit) for digit in num])
-    inp = inp.reshape(1, 10)
-    result = 0
-    if st.button("Predict"):
-        k = predict(param["W1"], param["W2"], param["b1"], param["b2"], inp)
-    st.success("The input is {}".format(k))
-    #if k == 1:
-      #st.write('Class1, Palindrome')
-    #else:
-      #st.write('Class0, Not Palindrome')
+    
+    num = st.chat_input("Enter number: ")
+    if num:
+        length_flag = True
+        if len(num) != 10:
+            length_flag = False
+        
+        binary_flag = True
+        for i in num:
+            if i not in [0,1]:
+                binary_flag = False
+                #st.write('Input is not binary, enter again')
+        
+        if not binary_flag or not length_flag:
+            st.write("Invalid Input")
+        else:
+            inp = np.array([int(digit) for digit in num])
+            inp = inp.reshape(1, 10)
+            #if st.button("Predict"):
+            k = predict(param["W1"], param["W2"], param["b1"], param["b2"], inp)
+            #if k == 1:
+              # k = 'Palindrome, hurray!'
+            #else:
+                #k = 'Not Palindrome'
+            #st.success("The input is {}".format(k))
+            if k == 1:
+              st.write('Class1, Palindrome')
+            else:
+              st.write('Class0, Not Palindrome')
+              
 
 
 if __name__ == '__main__':
